298f8d5c4499d2b42a171b4da6d1cf6b0d04eea1
ducvinh-dev <ldvinh596@gmail.com>
2024-02-28 22:21:07 +0700

    test used URL string instead of env

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -19,7 +19,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<any>) 
   req.headers.cookie = ''
 
   proxy.web(req, res, {
-    target: process.env.API_URL,
+    target: 'https://js-post-api.herokuapp.com',
     changeOrigin: true,
     selfHandleResponse: false
   })
